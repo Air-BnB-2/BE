924438b3bf3e4e2ae30ba7d5e56ca4bff30eee4d
Blayze <blayzestone@protonmail.com>
2020-06-20 11:24:12 -0700

    test: findById() helper method

--- users/userModel.spec.js
+++ users/userModel.spec.js
@@ -21,3 +21,35 @@ describe("add()", () => {
     expect(actual).toEqual(expected);
   });
 });
+
+describe("findById()", () => {
+  beforeAll(async () => {
+    await db("user").truncate();
+  });
+
+  it("should not find user with the id and return a falsy value", async () => {
+    const id = 1;
+
+    const actual = await USER.findById(id);
+
+    expect(actual).toBeFalsy();
+  });
+
+  it("should find the user with id and return it", async () => {
+    const user = {
+      username: "foobar",
+      password: "foobar",
+    };
+    const id = 1;
+
+    await USER.add(user);
+
+    const actual = await USER.findById(id);
+    expected = {
+      ...user,
+      id,
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
