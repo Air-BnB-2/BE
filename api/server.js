8d29dc7ccd227466cabcce06ccf35a9dbcff3736
Blayze <blayzestone@protonmail.com>
2020-06-22 09:43:45 -0700

    feat setup user authentication routes

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const helmet = require("helmet");
 
 const server = express();
 
+const authRouter = require("../auth/authRouter");
+
 server.use(helmet());
 server.use(express.json());
+server.use("/api/auth", authRouter);
 
 module.exports = server;
