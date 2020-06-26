345bf4dfabe048efa59dcce75df4a0c15284c035
Blayze <blayzestone@protonmail.com>
2020-06-26 10:11:42 -0700

    fix add preflight headers for cors

--- api/server.js
+++ api/server.js
@@ -11,6 +11,16 @@ const listingRouter = require("../listings/listingRouter");
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use("/api/auth", authRouter);
 server.use(
   "/api/:id/listings",
